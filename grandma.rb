99455c946fe20543d91e2eefd1905ed96cde26b5
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-03 00:14:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,6 +4,8 @@ def speak_to_grandma(phrase)
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
     "NO, NOT SINCE 1938!"
+  else
+    "HUH?! SPEAK UP, SONNY!"
   end
 end
 # Whatever you say to grandma, she should respond with
